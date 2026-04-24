4423cd61b04ee6bdbe9a27a16801414f7e7e9772
pedro-kellogg <pedro.albuquerque@kellogg.northwestern.edu>
2026-04-24 14:24:47 +0000

    code-along 1 and 2 solved

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,36 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3 + 4
+puts 3 * 4
+puts 3 - 4
+puts 5 / 2 # because you entered two integers, the computers understand you want an integer output
+puts 5 / 2.0
+puts 5 / 2.0
 
 # Non-numbers
+puts "hello, world!"
 
 # True, False
 
 # Nothing
 
 # Variables
+x = 2
+y = 5
+puts x * y
+
+x = x +1 # can autoincrement a variable
+puts x
+
+x+=1 # shortcut
+puts x
+
+name = "Pedro"
+puts "Hello, " + name + "!"
+puts "Hello, #{name}!" # easier, quicker, cleaner
 
 # Combine strings and variables
 
 # String manipulation
+puts name.upcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,70 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# True
+# False
 
 # Boolean Expressions
+# == equality
+# != inequal
+# >=, > greater than (or equal to)
+# >=, < lesser than (or equal to)
+x = 3
+y = 4
+puts x == y
+puts x != y
+puts x > y
+puts x < y
+puts x >= y
+puts x <= y
 
 # If Conditional Logic
+puts "================"
+
+# if condition
+#  ... do something
+# end
+
+if x == y
+  puts "x == y, yay!"
+end
+
+if x != y
+  puts "phew, math works"
+end
 
 # If/Else Conditional Logic
+if x == y
+  puts "x == y, yay!"
+else
+  puts "phew, math works"
+end
 
 # Elsif Conditional Logic
+home_team = 3
+away_team = 3
+
+# if home beats away!
+#   "WE WON!!"
+# if home loses!
+#   "WE LOST...boooooo"
+#if home and away tie
+#   "could be worse...."
+
+# if home beats away
+#   "WE WON!!"
+if home_team > away_team
+  puts "WE WON!!"
+# if home loses!
+#   "WE LOST...boooooo"
+elsif home_team < away_team
+  puts "WE LOST...boooooo"
+#if home and away tie
+#   "could be worse...."
+else
+  puts "could be worse...."
+end
 
 # Combining Expressions
+# && is AND
+# || is OR
